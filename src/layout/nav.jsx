c39367755e518a6e156942992189a18e61c608f5
siiaaa <121426629+siiaaa@users.noreply.github.com>
2022-12-26 15:33:42 +0530

    updated nav and footer with daisyui

--- src/layout/nav.jsx
+++ src/layout/nav.jsx
@@ -4,16 +4,37 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <>
-      
-          <Link to="/" activeStyle>
+
+<div className="navbar bg-base-300 shadow">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li>  <Link to="/" >
             Home
-          </Link>
-          <Link to="/contact" activeStyle>
+          </Link></li>
+        <li> <Link to="/contact">
             Contact Us
-          </Link>
-          <Link to="/join" activeStyle>
+          </Link></li>
+        <li>  <Link to="/join"  >
             Join
-          </Link>
+          </Link></li>
+      </ul>
+    </div>
+  </div>
+  <div className="navbar-center">
+  <Link to="/"  >  <a className="btn btn-ghost normal-case text-xl">Cool Community X</a>  </Link>
+  </div>
+  <div className="navbar-end">
+    <a className="btn" href="https://github.com/coolcommunityx">Get started</a>
+  </div>
+</div>
+      
+         
+          
+         
         
         
     </>
