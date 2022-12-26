dcb81b43f4b977062fc7dd8ddb6978c167646f2d
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2022-12-26 11:24:01 +0530

    Add files via upload

--- src/layout/nav.jsx
+++ src/layout/nav.jsx
@@ -1,1 +1,23 @@
-
+import React from "react";
+import { Link } from "react-router-dom";
+  
+const Nav = () => {
+  return (
+    <>
+      
+          <Link to="/" activeStyle>
+            Home
+          </Link>
+          <Link to="/contact" activeStyle>
+            Contact Us
+          </Link>
+          <Link to="/join" activeStyle>
+            Join
+          </Link>
+        
+        
+    </>
+  );
+};
+  
+export default Nav;
