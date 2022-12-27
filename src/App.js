fa878edb69be47db01e45f600cdcb7c7659c2e66
Ajinkgupta <76146259+Ajinkgupta@users.noreply.github.com>
2022-12-27 10:26:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css'; 
-import { BrowserRouter as Router, Routes, Route}
-    from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/home'; 
 import Nav from './layout/nav'; 
 import Footer from './layout/footer'; 
 import Join from './pages/Join';
-import Contact from './pages/contact';
-  
+import Contact from './pages/contact'; 
 function App() {
 return (
     <Router>
@@ -24,5 +22,4 @@ return (
     </Router>
 );
 }
-  
 export default App;
