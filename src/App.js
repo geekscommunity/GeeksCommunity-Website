d4d3a6da4ec5678e6219eb1d26de7bd9ed251716
Ajinkgupta <76146259+Ajinkgupta@users.noreply.github.com>
2022-12-28 19:21:43 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css'; 
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
 import Home from './pages/home'; 
 import Nav from './layout/nav'; 
 import Footer from './layout/footer'; 
 import Join from './pages/Join';
-import Contact from './pages/contact'; 
+import Contact from './pages/contact';
+  
 function App() {
 return (
     <Router>
     <Nav />
-    <div className="pt-16">
+    <div >
     <Routes>
         <Route exact path='/'  element={<Home />} />
         <Route path='/join' element={<Join/>} />
@@ -22,4 +24,5 @@ return (
     </Router>
 );
 }
+  
 export default App;
