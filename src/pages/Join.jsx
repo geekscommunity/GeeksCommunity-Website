2642a659e35e54937b531b6903063084c1e15729
Ajinkgupta <76146259+Ajinkgupta@users.noreply.github.com>
2022-12-28 19:21:08 +0530

    Add files via upload

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,12 +1,35 @@
-import React from "react";
-  
+import React from "react"; 
+import { Link } from "react-router-dom";
 const Join = () => {
   return (
     <div>
-      <h1>
-   Join Cool Community X
-      </h1>
+
+
+<div className="hero min-h-screen bg-base-200">
+    <div className="hero-content text-center content-center">
+     <div className="">
+     <img className="mask mask-squircle mx-auto w-32" src="https://cdn.jsdelivr.net/gh/geekscommunity/.github@main/profile/logo.png" />
+        <h1 className="text-3xl sm:text-5xl text-white font-bold">Join Geeks Events</h1>
+        <p className="py-6 text-[1/2]xl sm:text-2xl"> To boost your skills Geeks Community organises various events to participate in and earn cool Badges .  </p> 
+
+        <div className=" btn-group-horizontal ">
+        <Link to="/"  >  <button className="btn  mr-4 glass gap-2 bg-success text-white rounded-xl">
+   Home</button> </Link>
+   <Link to="/events" >
+  <button className="btn  gap-2 bg-primary ml-4 glass text-white rounded-xl">
+   Events</button> </Link>
+</div>
+
+
+      </div>
     </div>
+    
+  </div> 
+
+
+  
+    </div>
+  
   );
 };
   
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,37 @@
-import React from "react";
-  
+import React from "react"; 
 const Home = () => {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div>
+
+
+<div className="hero min-h-screen bg-base-200">
     <div className="hero-content text-center content-center">
-     <div className="max-w-md">
-     <img className="mask mask-circle mx-auto w-32" src="https://avatars.githubusercontent.com/u/121399569?s=200&v=4" />
-        <h1 className="text-5xl font-bold">Welcome To Cool Community X</h1>
-        <p className="py-6"> Cool Community X is a Coding socity where you can enjoy coding and staring projects</p>
-        <button className="btn btn-primary">Get Started</button>
+     <div className="">
+     <img className="mask mask-squircle mx-auto w-32" src="https://cdn.jsdelivr.net/gh/geekscommunity/.github@main/profile/logo.png" />
+        <h1 className="text-3xl sm:text-5xl text-white font-bold">Join  Geeks Community</h1>
+        <p className="py-6 text-[1/2]xl sm:text-2xl"> Join or create coding clubs, interact with experts, explore, teach your skills to other members , participate in hackathons , get internships, and join  Geeks community all for free!  <br/>What are you waiting for?
+        </p> 
+
+        <div className=" btn-group-horizontal ">
+  <a href="https://chat.whatsapp.com/HJHJYCGqcmB1MQ9plJ9PuB"> <button className="btn  mr-4 glass gap-2 bg-success text-white rounded-xl">
+  <svg viewBox="0 0 32 32" className="h-6 w-6"><path d=" M19.11 17.205c-.372 0-1.088 1.39-1.518 1.39a.63.63 0 0 1-.315-.1c-.802-.402-1.504-.817-2.163-1.447-.545-.516-1.146-1.29-1.46-1.963a.426.426 0 0 1-.073-.215c0-.33.99-.945.99-1.49 0-.143-.73-2.09-.832-2.335-.143-.372-.214-.487-.6-.487-.187 0-.36-.043-.53-.043-.302 0-.53.115-.746.315-.688.645-1.032 1.318-1.06 2.264v.114c-.015.99.472 1.977 1.017 2.78 1.23 1.82 2.506 3.41 4.554 4.34.616.287 2.035.888 2.722.888.817 0 2.15-.515 2.478-1.318.13-.33.244-.73.244-1.088 0-.058 0-.144-.03-.215-.1-.172-2.434-1.39-2.678-1.39zm-2.908 7.593c-1.747 0-3.48-.53-4.942-1.49L7.793 24.41l1.132-3.337a8.955 8.955 0 0 1-1.72-5.272c0-4.955 4.04-8.995 8.997-8.995S25.2 10.845 25.2 15.8c0 4.958-4.04 8.998-8.998 8.998zm0-19.798c-5.96 0-10.8 4.842-10.8 10.8 0 1.964.53 3.898 1.546 5.574L5 27.176l5.974-1.92a10.807 10.807 0 0 0 16.03-9.455c0-5.958-4.842-10.8-10.802-10.8z" fill-rule="evenodd"></path></svg>
+  Whatsapp</button> </a>
+    
+  <a href="https://discord.gg/tc7ZMtzy3K"><button className="btn  gap-2 bg-primary ml-4 glass text-white rounded-xl">
+  <svg  xmlns="http://www.w3.org/2000/svg"  className="h-6 w-6" viewBox="0 0 16 16"> <path d="M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z" fill="white"></path> </svg>
+    Discord</button> </a>
+</div>
+
+
       </div>
     </div>
-  </div>
+    
+  </div> 
+
+
+  
+    </div>
+  
   );
 };
   
